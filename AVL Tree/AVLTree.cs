06f44c8b4f4f60b48e9ddf648d9cabdb8588cfdb
Noah Ford <143020832+NoahFordGit@users.noreply.github.com>
2026-04-20 13:10:30 -0400

    Add project files.

---
+++ AVL Tree/AVLTree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4.AVL_Tree
+{
+    internal class AVLTree
+    {
+
+    }
+}
---
+++ AVL Tree/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4
+{
+    internal class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T>? Left { get; set; }
+        public Node<T>? Right { get; set; }
+
+        public Node(T data)
+        {
+            this.Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+}
